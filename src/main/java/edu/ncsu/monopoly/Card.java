52a1c1d5bf829ad0c8c9225eee4601a152a132f0
maximilianoaraujogalan <maximilianoaraujogalan@localhost>
2024-09-26 16:41:50 +0200

    cambio all, card 2

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -5,7 +5,7 @@ public abstract class Card {
  //Added comment 
     public static final int TYPE_CHANCE = 1;
     public static final int TYPE_CC = 2;
-
+//New cambio
     public abstract String getLabel();
     public abstract void applyAction();
     public abstract int getCardType();

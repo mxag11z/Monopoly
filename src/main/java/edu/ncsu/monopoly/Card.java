35574c368d3522c33002a9de095dc59096f7fda5
maximilianoaraujogalan <maximilianoaraujogalan@localhost>
2024-09-26 16:43:26 +0200

    acciones pruebas

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -6,6 +6,7 @@ public abstract class Card {
     public static final int TYPE_CHANCE = 1;
     public static final int TYPE_CC = 2;
 //New cambio
+    //nuevo cambioooo
     public abstract String getLabel();
     public abstract void applyAction();
     public abstract int getCardType();
